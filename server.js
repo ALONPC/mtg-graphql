723042816a1f8ce283acf4a2dcfe3899b55a0027
ALONPC <alon.berrios@gmail.com>
2020-08-04 04:47:16 -0400

    remake

--- server.js
+++ server.js
@@ -1,54 +1,49 @@
-const { ApolloServer, gql } = require("apollo-server")
+const { ApolloServer, gql } = require("apollo-server");
 const axios = require("axios");
 const api = "https://api.magicthegathering.io/v1";
 
 const typeDefs = gql`
-    type Card {
-        multiverseid: Int
-        name: String!
-        cmc: Int
-        type: String
-        set: String
-        imageUrl: String
-    }
+  type Card {
+    multiverseid: Int
+    name: String!
+    cmc: Int
+    type: String
+    set: String
+    imageUrl: String
+  }
 
-    type Query {
-        cards: [Card]
-        getCard(multiverseid: Int!): Card
-    }
+  type Query {
+    cards: [Card]
+    getCard(multiverseid: Int!): Card
+  }
 `;
 
 const resolvers = {
-    Query: {
-        cards: () => {
-            return new Promise((resolve, reject) => {
-                axios.get(`${api}/cards`).then(res => {
-                    resolve(res.data.cards
-                    )
-                }).catch(err => { err })
-
-            })
-        },
-        getCard: (root, args) => {
-            return new Promise((resolve, reject) => {
-                const { multiverseid } = args
-                axios.get(`${api}/cards/${multiverseid}`).then(res => {
-                    console.log('TCL: res.data.card.name', res.data.card.name);
-                    resolve(res.data.card)
-                }).catch(err => {
-                    throw new Error(err)
-                })
-            })
-        },
-    }
-}
+  Query: {
+    cards: async (root, args) => {
+      return await axios
+        .get(`${api}/cards`)
+        .then((res) => res.data.cards)
+        .catch((err) => console.log(`error ocurred ${err}`));
+    },
+    getCard: async (root, args) => {
+      const { multiverseid } = args;
+      return await axios
+        .get(`${api}/cards/${multiverseid}`)
+        .then((res) => res.data.card)
+        .catch((err) => {
+          throw new Error(err);
+        });
+    },
+  },
+};
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    playground: true
-})
+  typeDefs,
+  resolvers,
+  playground: true,
+});
 
 server.listen().then(({ url }) => {
-    console.log(`Server ready at ${url}`);
-})
+  console.log(`Server ready at ${url}`);
+});
